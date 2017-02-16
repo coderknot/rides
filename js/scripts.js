3d4294774ddcedceb8587e45ab79f0b22a3b791d
Chris Carr and Xi Xia <xi@>
2017-02-16 14:50:29 -0800

    highlight rides based on short/tall, rotate ride image based on thrill score

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,59 @@
 // });
 
 $(document).ready(function() {
-  $("#animals").on('change', function() {
-    var animal = $("#animals").val();
+  $("#ride-form").submit(function(event) {
+    event.preventDefault();
+    $(".col-md-3").removeClass("highlight");
+    $(".panel-default").removeClass("rotate");
+    $("#ride-row").show();
+    $(".tooShortTall").hide();
 
-    if (animal==="Bears") {
-      $("#bears").show();
-      $(".panel-default").not("#bears").hide();
-    } else if (animal==="Birds"){
-      $("#birds").show();
-      $(".panel-default").not("#birds").hide();
 
-    } else if (animal==="Bees"){
-      $("#bees").show();
-      $(".panel-default").not("#bees").hide();
+    var height = parseInt($("#height").val());
+    console.log(height);
+    var heightComfort = parseInt($("#height-comfort-input").val());
+    var adventure = parseInt($("#adventure-input").val());
+    var thrill = heightComfort * adventure;
+    console.log(thrill);
 
+
+    if (height < 60) {
+      $("#ride-row").hide();
+      $(".tooShortTall").show();
+
+    } else if (height < 66 && thrill < 7){
+      $(".short").addClass("highlight");
+      $(".panel-default#rocket").addClass("rotate");
+
+    } else if (height < 66 && thrill >= 7){
+      $(".short").addClass("highlight");
+      $(".panel-default#space").addClass("rotate");
+
+    } else if (height < 72 && thrill < 5){
+      $(".short").addClass("highlight");
+      $(".tall").addClass("highlight");
+      $(".panel-default#rocket").addClass("rotate");
+
+    } else if (height < 72 && thrill > 4 && thrill < 9){
+      $(".short").addClass("highlight");
+      $(".tall").addClass("highlight");
+      $(".panel-default#force").addClass("rotate");
+
+    } else if (height < 72 && thrill > 8 && thrill < 12){
+      $(".short").addClass("highlight");
+      $(".tall").addClass("highlight");
+      $(".panel-default#space").addClass("rotate");
+
+    } else if (height < 72 && thrill > 11 ){
+      $(".short").addClass("highlight");
+      $(".tall").addClass("highlight");
+      $(".panel-default#hulk").addClass("rotate");
+
+    } else if (height < 84) {
+      $(".tall").addClass("highlight");
+    } else {
+      $("#ride-row").hide();
+      $(".tooShortTall").show();
     }
 
     });
